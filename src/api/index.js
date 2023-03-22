2751b4e043dfc0ea9f591d834751b6709653cf1f
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-03-22 16:38:55 +0530

    created custom function

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,50 @@
-   const customFetch = () => {
+   import { LOCALSTORAGE_TOKEN_KEY } from '../utils';
 
+   const customFetch = async (url, { body, ...customConfig }) => {
+   const token = window.localStorage.getItem(LOCALSTORAGE_TOKEN_KEY);
+
+   const headers = {
+      'content-type': 'application/json',
+      Accept: 'application/json',
+   };
+
+   if (token) {
+      headers.Authorization = `Bearer ${token}`;
+   }
+
+   const config = {
+      ...customConfig,
+      headers: {
+         ...headers,
+         ...customConfig.headers,
+      },
+   };
+
+   if (body) {
+      config.body = JSON.stringify(body);
+   }
+
+   try {
+
+      const response = await fetch(url, config);
+      const data = await response.json();
+
+      if (response.success) {
+         return {
+         data: data.data,
+         success: true,
+         };
+      }
+
+      throw new Error(data.message);
+      
+   } catch (error) {
+      console.error('error');
+      return {
+         message: error.message,
+         success: false,
+      };
+   }
    };
 
    const getPosts = (page, limit) => {

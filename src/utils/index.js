f13e2b52e9d20a88ef860bea090de3a1837fd056
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-03-22 16:38:04 +0530

    creating localStorage token key

--- src/utils/index.js
+++ src/utils/index.js
@@ -21,3 +21,5 @@
    userInfo: (userId) => `${API_ROOT}/users/${userId}`,
    searchUsers: (searchText) => `${API_ROOT}/users/search?text=${searchText}`,
    };
+
+   export const LOCALSTORAGE_TOKEN_KEY = '__codeial_tokan__';

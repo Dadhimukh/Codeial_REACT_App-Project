d511487605e26fd27f55f62c3779e0ea56ca7ae2
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-03-22 17:05:36 +0530

    importing getPost and using useEffect, and in useEffect we are getting data from calling getPost(), and showing the data in console

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,16 @@
+import { useEffect } from 'react';
+import { getposts } from '../api/index.js';
+
 function App() {
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await getposts();
+      console.log('response', response);
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <div className="App">
       <h1> Hello World</h1>
